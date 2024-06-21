ccc4e8c07e5a79f8d6768aa92f1515fa81515164
BrenoCGuimaraes <breno.costa.guimaraes@gmail.com>
2024-06-21 18:47:58 -0300

    tudo sincronizado pae

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -7,6 +7,7 @@ export default function Home() {
             <h1>Pagina Inicial</h1>
             <h2>Tech Insights</h2>
             <h3>Bylord teste</h3>
+            <h4>B2 teste</h4>
         </>
     )
 }
