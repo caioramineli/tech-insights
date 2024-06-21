daa6aa61f24b38e17e901e6d5fb901befaeee334
caioramineli <caiofrancoramineli3@gmail.com>
2024-06-21 18:45:11 -0300

    ajuste de sincronia

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/Header"
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <h1>Pagina Inicial</h1>
+            <h2>Tech Insights</h2>
+            <h3>Bylord teste</h3>
+        </>
+    )
+}
