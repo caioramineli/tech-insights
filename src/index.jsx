2a9c794b2f56c0236f5007d12c7b053b36efd9f2
caioramineli <caiofrancoramineli3@gmail.com>
2024-06-21 17:37:38 -0300

    Header finalizado

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import MinhasRotas from './minhasRotas';
+import "./globalStyle.css";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <>
+        <MinhasRotas />
+    </>
+);
---
+++ src/pages/login/index.jsx
@@ -0,0 +1,10 @@
+import Header from "../../components/Header"
+
+export default function Login() {
+    return (
+        <>
+            <Header/>
+            <h1>Tela de login!</h1>
+        </>
+    )
+}
