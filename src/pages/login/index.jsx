1da812f1a83b21e99ff50529cb3385a2e7539b8c
BrenoCGuimaraes <breno.costa.guimaraes@gmail.com>
2024-06-25 17:14:57 -0300

    tela de login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,10 +1,24 @@
 import Header from "../../components/Header"
+import user from "../../assets/user.png"
+import './style.css';
 
 export default function Login() {
     return (
         <>
             <Header/>
-            <h1>Tela de login!</h1>
+            <div id="container-login">
+                <form>
+                    <img src={user} alt="user icon" id="user-login" />
+                    <h1>Login</h1>
+                        <p>Email</p>
+                        <input type="text" placeholder="Insira seu email"/>
+                        <p>Senha</p>
+                        <input type="text" placeholder="Insira sua senha"/>
+                    <a href="">Esqueceu a senha ?</a>
+                    <button id="login-entrar">Entrar</button>
+                    <button id="login-criar">Criar Conta</button>
+                </form>
+            </div>
         </>
     )
 }
