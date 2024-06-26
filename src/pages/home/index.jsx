ea652435c42bff68489f1bbbd82f23cca5022ec2
caioramineli <caiofrancoramineli3@gmail.com>
2024-06-26 17:25:30 -0300

    criação da sessão de produtos e ajuste no footer

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,67 @@
+import "./style.css"
+import rtx3060 from "../../assets/3060MSI.jpg"
+
 export default function Home() {
     return (
         <>
             <h1>Pagina Inicial</h1>
-            <h2>Tech Insights</h2>
-            <h3>Bylord teste</h3>
-            <h4>B2 teste</h4>
+            <section className="containerProdutos">
+                <div className="boxProduto">
+                    <img src={rtx3060} alt="RTX 3060 MSI" />
+                    <p>Placa de Vídeo RTX 3060 Ventus 2X MSI NVIDIA GeForce, 12GB GDDR6, DLSS, Ray Tracing - RTX 3060 Ventus 2X 12G OC</p>
+                    <p>R$ 1750,00 à vista</p>
+                    <p>9x de R$ 220,20</p>
+                </div>
+
+                <div className="boxProduto">
+                    <img src={rtx3060} alt="RTX 3060 MSI" />
+                    <p>Placa de Vídeo RTX 3060 Ventus 2X MSI NVIDIA GeForce, 12GB GDDR6, DLSS, Ray Tracing - RTX 3060 Ventus 2X 12G OC</p>
+                    <p>R$ 1750,00 à vista</p>
+                    <p>9x de R$ 220,20</p>
+                </div>
+
+                <div className="boxProduto">
+                    <img src={rtx3060} alt="RTX 3060 MSI" />
+                    <p>Placa de Vídeo RTX 3060 Ventus 2X MSI NVIDIA GeForce, 12GB GDDR6, DLSS, Ray Tracing - RTX 3060 Ventus 2X 12G OC</p>
+                    <p>R$ 1750,00 à vista</p>
+                    <p>9x de R$ 220,20</p>
+                </div>
+
+                <div className="boxProduto">
+                    <img src={rtx3060} alt="RTX 3060 MSI" />
+                    <p>Placa de Vídeo RTX 3060 Ventus 2X MSI NVIDIA GeForce, 12GB GDDR6, DLSS, Ray Tracing - RTX 3060 Ventus 2X 12G OC</p>
+                    <p>R$ 1750,00 à vista</p>
+                    <p>9x de R$ 220,20</p>
+                </div>
+
+                <div className="boxProduto">
+                    <img src={rtx3060} alt="RTX 3060 MSI" />
+                    <p>Placa de Vídeo RTX 3060 Ventus 2X MSI NVIDIA GeForce, 12GB GDDR6, DLSS, Ray Tracing - RTX 3060 Ventus 2X 12G OC</p>
+                    <p>R$ 1750,00 à vista</p>
+                    <p>9x de R$ 220,20</p>
+                </div>
+                
+                <div className="boxProduto">
+                    <img src={rtx3060} alt="RTX 3060 MSI" />
+                    <p>Placa de Vídeo RTX 3060 Ventus 2X MSI NVIDIA GeForce, 12GB GDDR6, DLSS, Ray Tracing - RTX 3060 Ventus 2X 12G OC</p>
+                    <p>R$ 1750,00 à vista</p>
+                    <p>9x de R$ 220,20</p>
+                </div>
+
+                <div className="boxProduto">
+                    <img src={rtx3060} alt="RTX 3060 MSI" />
+                    <p>Placa de Vídeo RTX 3060 Ventus 2X MSI NVIDIA GeForce, 12GB GDDR6, DLSS, Ray Tracing - RTX 3060 Ventus 2X 12G OC</p>
+                    <p>R$ 1750,00 à vista</p>
+                    <p>9x de R$ 220,20</p>
+                </div>
+
+                <div className="boxProduto">
+                    <img src={rtx3060} alt="RTX 3060 MSI" />
+                    <p>Placa de Vídeo RTX 3060 Ventus 2X MSI NVIDIA GeForce, 12GB GDDR6, DLSS, Ray Tracing - RTX 3060 Ventus 2X 12G OC</p>
+                    <p>R$ 1750,00 à vista</p>
+                    <p>9x de R$ 220,20</p>
+                </div>
+            </section>
         </>
     )
 }
