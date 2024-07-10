a656b3ae35ccf5c1e529475b72159fdc64b8edce
caioramineli <caiofrancoramineli3@gmail.com>
2024-07-10 00:17:30 -0300

    ajustes na página do carrinho e melhorias gerais

--- src/pages/carrinho/index.jsx
+++ src/pages/carrinho/index.jsx
@@ -15,46 +15,59 @@ export default function Carrinho() {
                         <th>Preço</th>
                     </tr>
                     <tr>
-                        <td><img src={rtx3060} alt="3060" /> <p>Placa de Vídeo RTX 3060 Ventus 2X MSI NVIDIA GeForce, 12GB GDDR6, DLSS, Ray Tracing - RTX 3060 Ventus 2X 12G OC.</p></td>
-                        <td>1</td>
-                        <td>1.500,00</td>
+                        <td>
+                            <div className="boxImgDescricao">
+                                <img src={rtx3060} alt="3060" />
+                                <p>Placa de Vídeo RTX 3060 Ventus 2X MSI NVIDIA GeForce, 12GB GDDR6, DLSS, Ray Tracing - RTX 3060 Ventus 2X 12G OC.</p>
+                            </div>
+                        </td>
+                        <td className="boxQuantidade">1</td>
+                        <td className="boxPreco">1.500,00</td>
                     </tr>
                     <tr>
-                        <td><img src={rtx3060} alt="3060" /> <p>Placa de Vídeo RTX 3060 Ventus 2X MSI NVIDIA GeForce, 12GB GDDR6, DLSS, Ray Tracing - RTX 3060 Ventus 2X 12G OC.</p></td>
-                        <td>2</td>
-                        <td>3.000,00</td>
+                        <td>
+                            <div className="boxImgDescricao">
+                                <img src={rtx3060} alt="3060" />
+                                <p>Placa de Vídeo RTX 3060 Ventus 2X MSI NVIDIA GeForce, 12GB GDDR6, DLSS, Ray Tracing - RTX 3060 Ventus 2X 12G OC.</p>
+                            </div>
+                        </td>
+                        <td className="boxQuantidade">2</td>
+                        <td className="boxPreco">3.000,00</td>
                     </tr>
                 </table>
                 <div className="limparCarrinho">
                     <FaTrash />
                     <h3>Limpar carrinho</h3>
                 </div>
-                <div className="cupomDesconto">
-                    <h3>Cupom de desconto</h3>
-                    <input type="text" />
-                    <button>Aplicar</button>
-                </div>
-                <div className="calculoFrete">
-                    <h3>Calcular Frete</h3>
-                    <input type="text" />
-                    <button>Calcular</button>
+                <div className="containerCupomFrete">
+                    <div className="cupomDesconto">
+                        <h3>Cupom de desconto</h3>
+                        <input type="text" />
+                        <button>Aplicar</button>
+                    </div>
+                    <div className="calculoFrete">
+                        <h3>Calcular Frete</h3>
+                        <input type="text" />
+                        <button>Calcular</button>
+                    </div>
                 </div>
             </section>
 
-            <section className="resumoCarrinho">
+            <section className="containerResumo">
                 <h2>Resumo</h2>
-                <div>
-                    <p>Valor do carrinho</p>
-                    <p>Descontos</p>
-                    <p>Frete</p>
-                    <p>Valor Total:</p>
-                </div>
-
-                <div>
-                    <p>R$ 0,00</p>
-                    <p>R$ 0,00</p>
-                    <p>R$ 0,00</p>
-                    <p>R$ 0,00</p>
+                <div className="resumoCarrinho">
+                    <div>
+                        <p>Valor do carrinho:</p>
+                        <p>Descontos:</p>
+                        <p>Frete:</p>
+                        <p>Valor Total:</p>
+                    </div>
+                    <div>
+                        <p>R$ 0,00</p>
+                        <p>R$ 0,00</p>
+                        <p>R$ 0,00</p>
+                        <p>R$ 0,00</p>
+                    </div>
                 </div>
 
                 <button>Finalizar Pedido</button>
