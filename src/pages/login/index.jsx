8f12b9f9ac642c19b4c61655bf248b124be62792
caioramineli <caiofrancoramineli3@gmail.com>
2024-07-19 18:28:32 -0300

    ajustes de css

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,24 +1,33 @@
-
-import user from "../../assets/user.png"
+import { MdOutlineEmail } from "react-icons/md";
 import './style.css';
+import { IoIosLock } from "react-icons/io";
 
 export default function Login() {
     return (
         <>
             <div id="container-login">
                 <form method="POST">
-                    <img src={user} alt="user icon" id="user-login" />
                     <h1>Login</h1>
 
-                    <label htmlFor='email'>Email:</label>
-                    <input name="email" type="text" placeholder="Insira seu email" />
+                    <div className="inputEmail">
+                        <MdOutlineEmail />
+                        <input name="email" type="text" placeholder="Insira seu email" />
+                    </div>
 
-                    <label htmlFor='senha'>Senha:</label>
-                    <input name='senha' type="text" placeholder="Insira sua senha" />
-                    <a href={'/'}>Esqueceu a senha?</a>
+                    <div className="inputSenha">
+                        <IoIosLock />
+                        <input name='senha' type="text" placeholder="Insira sua senha" />
+                    </div>
+
+                    <a id="esqueceuSenha" href='/'>Esqueceu a senha?</a>
+
+                    <span id="spanIncorreto">Email ou senha incorretos</span>
 
                     <button type="submit" id="btn-entrar">Entrar</button>
-                    <a id="a-btn-criar" href={'/cadastrar'}><button type="button" >Criar Conta</button></a>
+
+                    <a id="a-btn-criar" href="/cadastrar">
+                        <button type="button" >Criar Conta</button>
+                    </a>
                 </form>
             </div>
         </>
