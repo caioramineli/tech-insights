18e4ca47167b1f6048c2e472f65bf56f92b7d68b
BrenoCGuimaraes <breno.costa.guimaraes@gmail.com>
2024-09-05 22:48:25 -0300

    responsividade do produto

--- src/components/DescricaoProduto/index.jsx
+++ src/components/DescricaoProduto/index.jsx
@@ -4,7 +4,7 @@ export default function DescricaoProduto({ descricao }) {
     return (
         <div className='containerEspecificacaoEDescricao'>
             <h2 className='text-xl font-bold'>Descrição</h2>
-            <p>{descricao}</p>
+            <p className='text-sm md:text-base'>{descricao}</p>
         </div>
     );
 }
