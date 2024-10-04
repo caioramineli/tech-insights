27360620f909dae3541ee7163f5a641b09b03662
BrenoCGuimaraes <breno.costa.guimaraes@gmail.com>
2024-10-04 20:17:55 -0300

    minha-conta quase pronta

--- src/pages/conta/index.jsx
+++ src/pages/conta/index.jsx
@@ -7,6 +7,10 @@ import { FaRegAddressCard } from "react-icons/fa6";
 import { LiaLuggageCartSolid } from "react-icons/lia";
 import { FaRegHeart } from "react-icons/fa";
 import { FaMapLocationDot } from "react-icons/fa6";
+import { VscSmiley } from "react-icons/vsc";
+import { IoIosArrowForward } from "react-icons/io";
+
+import { MdAccessible } from "react-icons/md";
 
 import './style.css';
 import { Link } from 'react-router-dom';
@@ -44,53 +48,104 @@ export default function Conta() {
             </div>
         );
     }
+    const getFirstName = (fullName) => {
+        return fullName.split(' ')[0];
+    };
 
     return (
 
-        <div className='flex'>
-            <div className="conta-container">
-                <h1 className='flex text-xl justify-center font-bold py-2'>MINHA CONTA</h1>
-                <hr className='border border-teal-600 w-full ' />
-                <br />
-                <div className='grid grid-cols-2 gap-3'>
-                    <button className='flex flex-col items-center gap-1' onClick={() => updateToglle(1)}>
-                        <FaRegAddressCard className={toggle === 1 ? 'text-teal-600 text-4xl' : 'text-zinc-400 text-4xl'} />
-                        Meu cadastro
-                    </button>
+        <div className='flex flex-col w-[80%] max-w-[1300px] my-12'>
+            <h1 className='flex text-2xl justify-center font-bold py-2'>MINHA CONTA</h1>
+            <hr className='border border-teal-600 w-60 m-auto' />
 
-                    <Link to={'pedidos'}>
-                        <button className='flex flex-col items-center gap-1'>
-                            <LiaLuggageCartSolid className='text-4xl'/>
-                            Meu pedidos
-                        </button>
-                    </Link>
-
-                    <button className='flex flex-col items-center gap-1' onClick={() => updateToglle(3)}>
-                        <FaRegHeart className={toggle === 3 ? 'text-teal-600 text-4xl' : 'text-zinc-400 text-3xl'} />
-                        Favoritos
-                    </button>
-
-                    <button className='flex flex-col items-center gap-1' onClick={() => updateToglle(4)}>
-                        <FaMapLocationDot className={toggle === 4 ? 'text-teal-600 text-4xl' : 'text-zinc-400 text-4xl'} />
-                        Meus Endereços
-                    </button>
-
-
-
-
+            <div className='p-3 bsPadrao bg-white rounded-md gap-2 mt-4'>
+                <div className='flex flex-row'>
+                    <VscSmiley className='text-4xl' />
+                    <span className='text-xl p-1 font-bold text-nowrap'>Seja Bem-vindo, {getFirstName(user.nome)}</span>
                 </div>
-                <br />
-                <hr className='border border-teal-600 w-full' />
-                <div className={toggle === 1 ? 'grid items-center  gap-2 w-[95%] sm:w-11/12 m-auto' : 'hidden'}>
-                    <p><strong>Nome do Perfil:</strong> {userData.nome}</p>
-                    <p><strong>CPF:</strong> {userData.cpf}</p>
-                    <p><strong>Data de Nascimento:</strong> {userData.dataNascimento}</p>
-                    <p><strong>Telefone:</strong> {userData.telefone}</p>
-                    <p><strong>Email:</strong> {userData.email}</p>
+            </div>
+            <div className='grid grid-cols-4 gap-3 mt-3'>
+                <Link to={'dados'}>
+                    <div className='flex items-center justify-center p-5 border bsPadrao bg-white rounded-md gap-4 h-36'>
+                        <span>
+                            <FaRegAddressCard className='text-emerald-600 text-4xl' />
+                        </span>
+                        <div className='grid grid-cols-1 '>
+                            <h2 className='text-base font-bold'>MEUS DADOS</h2>
+                            <p className='text-sm sm-1'>Altere seus dados cadastrados. </p>
+                        </div>
+                    </div>
+                </Link>
+
+                <Link to={'pedidos'}>
+                    <div className='flex items-center justify-center p-5 border bsPadrao bg-white rounded-md gap-4 h-36'>
+                        <span>
+                            <LiaLuggageCartSolid className='text-emerald-600 text-4xl' />
+                        </span>
+                        <div className='grid grid-cols-1 '>
+                            <h2 className='text-base font-bold uppercase'>Meu pedidos</h2>
+                            <p className='text-sm sm-1'>Veja históricos e acompanhe suas compras.</p>
+                        </div>
+                    </div>
+                </Link>
+
+                <Link to={'favoritos'}>
+                    <div className='flex items-center justify-center p-5 border bsPadrao bg-white rounded-md gap-4 h-36'>
+                        <span>
+                            <FaRegHeart className='text-emerald-600 text-4xl' />
+                        </span>
+                        <div className='grid grid-cols-1 '>
+                            <h2 className='text-base font-bold uppercase'>Favoritos</h2>
+                            <p className='text-sm sm-1'>Veja seus Favoritos</p>
+                        </div>
+                    </div>
+                </Link>
+
+                <Link to={'enderecos'}>
+                    <div className='flex items-center justify-center p-5 border bsPadrao bg-white rounded-md gap-4 h-36'>
+                        <span>
+                            <FaMapLocationDot className='text-emerald-600 text-4xl' />
+                        </span>
+                        <div className='grid grid-cols-1 '>
+                            <h2 className='text-base font-bold uppercase'>Meus Endereços</h2>
+                            <p className='text-sm sm-1'> Veja seus endereços ou cadastre um novo endereço.</p>
+                        </div>
+                    </div>
+                </Link>
+            </div>
+            <hr className='border border-teal-600 w-full mt-4' />
+            <div className='flex flex-row justify-between mt-4'>
+                <h1 className='font-bold uppercase '>Resumo do seu ultimo pedido</h1>
+                <Link to={'pedidos'} className='uppercase underline decoration-solid'> ir para meus pedidos</Link>
+            </div>      
+            <div className='flex flex-col bsPadrao bg-white mt-4 rounded-md'>
+                <div className='gap-2 p-2 items-center '>
+                    <div className='flex p-1 gap-2 items-center'>
+                        <h1 className='font-bold uppercase'>Pedido:</h1>
+                        <p className=' items-center'> cod-pedido - 02/02/2022 </p>
+                        <Link className='flex ml-auto '>
+                            <button className='flex flex-row border bg-emerald-600 p-2 rounded-md items-center'>
+                                <h2 className='text-base uppercase text-teal-50'> ver Detalhes </h2>
+                                <IoIosArrowForward className='text-teal-50' />
+                            </button>
+                        </Link>
+                    </div>
                 </div>
-                <div className={toggle === 2 ? 'flex flex-col items-center gap-2 w-[95%] sm:w-11/12 m-auto' : 'hidden'}>
-                    <p className='font-bold'>Todos pedidos</p>
-                    <hr className='border border-teal-600 w-full' />
+                <hr />
+                <div className='p-3 gap-2 mt-1 items-center '>
+                    <h1 className='font-bold uppercase text-teal-600'>Aguardando o status do pedido</h1>
+                </div>
+                <hr />
+                <div className='p-3 gap-2 mt-1 items-center '>
+                    <h1>IMG  Pagamento via FORMA-PAGAMENTO</h1>
+                </div>
+                <hr />
+                <div className='flex flex-row p-3 gap-4 mt-1 items-center '>
+                    <MdAccessible className='text-6xl border bsPadrao bg-white '/>
+                    <div className='grid grid-cols-1 gap-1'>
+                        <h1 className='font-bold uppercase'>Caderante de primeira classe  </h1>
+                        <p>Quantidade: 0</p>
+                    </div>
                 </div>
             </div>
         </div>
