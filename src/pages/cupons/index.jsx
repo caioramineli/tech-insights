c414b02b77d1d10aa00fa07f68affc1e6914f3c8
caioramineli <caiofrancoramineli3@gmail.com>
2024-10-10 00:11:24 -0300

    Pagina de cupons finalizada

--- src/pages/cupons/index.jsx
+++ src/pages/cupons/index.jsx
@@ -1,51 +1,66 @@
 import { MdDiscount } from "react-icons/md";
-import { FaRegCopy } from "react-icons/fa6";
+import { FaRegCopy, FaCheck } from "react-icons/fa6";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Loading from "../../components/Loading";
+import { TbRuler3 } from "react-icons/tb";
 
 
 export default function PaginaCupons() {
+    const [cupons, setCupons] = useState([]);
+    const [loading, setLoading] = useState(TbRuler3);
+    const api = process.env.REACT_APP_API_URL;
+    const [copiedCode, setCopiedCode] = useState(null);
+
+    const handleCopy = (codigo) => {
+        navigator.clipboard.writeText(codigo);
+        setCopiedCode(codigo);
+        setTimeout(() => setCopiedCode(null), 2000);
+    };
+
+    useEffect(() => {
+        const buscarCupons = async () => {
+            try {
+                const response = await axios.get(`${api}cupon/listar`);
+                setCupons(response.data.cupons);
+            } catch (err) {
+                console.log(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+        buscarCupons();
+    }, [api]);
+
+    if (loading) {
+        return <Loading />;
+    }
+
     return (
         <div className="flex flex-col gap-4 w-[90%] xl:w-4/5 m-auto max-w-[1300px] my-8">
-            <div className="flex items-center">
+            <div className="flex items-center gap-2">
                 <MdDiscount className="text-2xl text-emerald-600" />
                 <h1 className="text-2xl font-bold text-zinc-900">Cupons Ativos</h1>
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <div className="flex flex-col gap-2 p-4 bg-emerald-700 rounded-lg">
-                    <h1 className="text-xl text-emerald-50 font-bold">5% OFF</h1>
-                    <p className="text-lg text-emerald-50">Válido na sua 1º compra</p>
-                    <button className="text-xl p-2 font-bold bg-white rounded-md relative">
-                        PRIMEIRA5
-                        <FaRegCopy className="absolute top-1 right-1" />
-                    </button>
-                </div>
-
-                <div className="flex flex-col gap-2 p-4 bg-emerald-700 rounded-lg">
-                    <h1 className="text-xl font-bold text-emerald-50">R$ 50,00 OFF</h1>
-                    <p className="text-lg text-emerald-50">Válido em copras acima de R$500</p>
-                    <button className="text-xl p-2 font-bold bg-white rounded-md relative">
-                        TECH50
-                        <FaRegCopy className="absolute top-1 right-1" />
-                    </button>
-                </div>
-
-                <div className="flex flex-col gap-2 p-4 bg-emerald-700 rounded-lg">
-                    <h1 className="text-xl text-emerald-50 font-bold">5% OFF</h1>
-                    <p className="text-lg text-emerald-50">Válido na sua 1º compra</p>
-                    <button className="text-xl p-2 font-bold bg-white rounded-md relative">
-                        PRIMEIRA5
-                        <FaRegCopy className="absolute top-1 right-1" />
-                    </button>
-                </div>
-
-                <div className="flex flex-col gap-2 p-4 bg-emerald-700 rounded-lg">
-                    <h1 className="text-xl font-bold text-emerald-50">R$ 50,00 OFF</h1>
-                    <p className="text-lg text-emerald-50">Válido em copras acima de R$500</p>
-                    <button className="text-xl p-2 font-bold bg-white rounded-md relative">
-                        TECH50
-                        <FaRegCopy className="absolute top-1 right-1" />
-                    </button>
-                </div>
+                {cupons.map((cupon) => (
+                    <div className="flex flex-col gap-1 p-4 bg-emerald-700 rounded-lg shadow-md" key={cupon.codigo}>
+                        <h1 className="text-xl text-emerald-50 font-bold">
+                            {cupon.tipo === "percentual" ? `${cupon.valor}% OFF` : `R$ ${cupon.valor} OFF`}
+                        </h1>
+                        <p className="text-lg text-emerald-50">{cupon.descricao}</p>
+                        <hr className="border-dashed border my-2" />
+                        <button onClick={() => handleCopy(cupon.codigo)} className="text-xl p-2 font-bold bg-white rounded-md relative text-zinc-900">
+                            {cupon.codigo}
+                            {copiedCode === cupon.codigo ? (
+                                <FaCheck className="absolute top-1 right-1 text-zinc-900" />
+                            ) : (
+                                <FaRegCopy className="absolute top-1 right-1 text-zinc-900" />
+                            )}
+                        </button>
+                    </div>
+                ))}
             </div>
         </div>
     )
